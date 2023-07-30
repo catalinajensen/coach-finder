dfefff9f603467089a8387d2be9b4efea1d29de8
Catalina Jensen <catalinabjensen@gmail.com>
2023-07-30 23:39:06 +0200

    Filter coaches

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,5 +1,5 @@
 <template>
-  <section>FILTER</section>
+  <section><coach-filter @change-filter="setFilters"></coach-filter></section>
   <section>
     <base-card>
       <div class="controls">
@@ -14,8 +14,7 @@
           :last-name="coach.lastName"
           :rate="coach.hourlyRate"
           :areas="coach.areas"
-        ></coach-item
-        >>
+        ></coach-item>
       </ul>
       <h3 v-else>No coaches found.</h3>
     </base-card>
@@ -24,16 +23,44 @@
 
 <script>
 import CoachItem from '../../components/coaches/CoachItem.vue'
+import CoachFilter from '../../components/coaches/CoachFilter.vue'
 export default {
   components: {
-    CoachItem
+    CoachItem,
+    CoachFilter
+  },
+  data() {
+    return {
+      activeFilters: {
+        frontend: true,
+        backend: true,
+        career: true
+      }
+    }
   },
   computed: {
     filteredCoaches() {
-      return this.$store.getters['coaches/coaches']
+      const coaches = this.$store.getters['coaches/coaches']
+      return coaches.filter((coach) => {
+        if (this.activeFilters.frontend && coach.areas.includes('frontend')) {
+          return true
+        }
+        if (this.activeFilters.backend && coach.areas.includes('backend')) {
+          return true
+        }
+        if (this.activeFilters.career && coach.areas.includes('career')) {
+          return true
+        }
+        return false
+      })
     },
     hasCoaches() {
       return this.$store.getters['coaches/hasCoaches']
+    }
+  },
+  methods: {
+    setFilters(updatedFilters) {
+      this.activeFilters = updatedFilters
     }
   }
 }
