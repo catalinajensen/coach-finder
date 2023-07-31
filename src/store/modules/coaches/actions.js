570f0d596ad09f4e9341fee261ece29a6cf34bee
Catalina Jensen <catalinabjensen@gmail.com>
2023-07-31 13:55:24 +0200

    Add token to outgoing requests

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -9,8 +9,11 @@ export default {
       areas: data.areas
     }
 
+    const token = context.rootGetters.token
+
     const response = await fetch(
-      `https://vue-http-demo-8ad6c-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      `https://vue-http-demo-8ad6c-default-rtdb.firebaseio.com/coaches/${userId}.json?auth=` +
+        token,
       {
         method: 'PUT',
         body: JSON.stringify(coachData)
