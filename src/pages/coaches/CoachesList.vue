d0b8f923f80ff5b844271f5a9cf893e9f780daa9
Catalina Jensen <catalinabjensen@gmail.com>
2023-07-31 14:02:01 +0200

    Update UI

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -8,7 +8,8 @@
       <base-card>
         <div class="controls">
           <base-button mode="outline" @click="loadCoaches(true)">Refresh</base-button>
-          <base-button v-if="!isCoach && !isLoading" link to="/register"
+          <base-button v-if="!isLoggedIn" link to="/auth">Login</base-button>
+          <base-button v-if="!isCoach && !isLoading && isLoggedIn" link to="/register"
             >Register as Coach</base-button
           >
         </div>
@@ -52,6 +53,9 @@ export default {
     }
   },
   computed: {
+    isLoggedIn() {
+      return this.$store.getters.isAuthenticated
+    },
     isCoach() {
       return this.$store.getters['coaches/isCoach']
     },
