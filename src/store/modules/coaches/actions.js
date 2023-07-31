52045f0e778389a4a68739ef459ebdd2a8b86415
Catalina Jensen <catalinabjensen@gmail.com>
2023-07-31 10:35:08 +0200

    Add error handling

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -36,7 +36,7 @@ export default {
     const responseData = await response.json()
 
     if (!response.ok) {
-      // error ...
+      const error = new Error(responseData.message || 'Failed to fetch!')
     }
 
     const coaches = []
