4138cd540d70be9e1d87ee62b449ddfb8db35bf2
Catalina Jensen <catalinabjensen@gmail.com>
2023-07-31 10:21:34 +0200

    Add spinner

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -4,9 +4,14 @@
     <base-card>
       <div class="controls">
         <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
-        <base-button v-if="!isCoach" link to="/register">Register as Coach</base-button>
+        <base-button v-if="!isCoach && !isLoading" link to="/register"
+          >Register as Coach</base-button
+        >
       </div>
-      <ul v-if="hasCoaches">
+      <div v-if="isLoading">
+        <base-spinner></base-spinner>
+      </div>
+      <ul v-else-if="hasCoaches">
         <coach-item
           v-for="coach in filteredCoaches"
           :key="coach.id"
@@ -32,6 +37,7 @@ export default {
   },
   data() {
     return {
+      isLoading: false,
       activeFilters: {
         frontend: true,
         backend: true,
@@ -59,7 +65,7 @@ export default {
       })
     },
     hasCoaches() {
-      return this.$store.getters['coaches/hasCoaches']
+      return !this.isLoading && this.$store.getters['coaches/hasCoaches']
     }
   },
   created() {
@@ -69,8 +75,10 @@ export default {
     setFilters(updatedFilters) {
       this.activeFilters = updatedFilters
     },
-    loadCoaches() {
-      this.$store.dispatch('coaches/loadCoaches')
+    async loadCoaches() {
+      this.isLoading = true
+      await this.$store.dispatch('coaches/loadCoaches')
+      this.isLoading = false
     }
   }
 }
